e00befd810f2b3068f98ea4c8056efa853387282
devin <devin@devins-MacBook-Pro.local>
2022-11-10 16:07:53 -0500

    updated nav and about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,12 +3,19 @@ import classes from './About.module.css';
 function About() {
     return ( 
         <div className={classes.aboutBG}>
-        <div className={classes.About}>
-            <div className={classes.Container}>
+        <div className={classes.about}>
+            <div className={classes.container}>
                 <h1 className={classes.aboutMe}>About Me</h1>
-                <p>I am an...</p>
+                <p>
+                    I am a novice Full Stack Developer, with experience in building and creating Web Applications.
+                    I am working on receiving a Certificate in Full Stack Web Development from Rutgers University,
+                    where I have gained proficiency in both front-end and back-end development. 
+                </p>
+                <p className={classes.paragraph}>
+                    I am well organized and extremely detail-oriented, which allows me to focus
+                    on the task at hand and get the job done efficiently and professionally. 
+                </p>
             </div>
-            {/* <img className={classes.aboutImage} src={aboutBG} alt="highway"></img> */}
         </div>
         </div>
      );
